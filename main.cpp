b9639d52b83b1ed88af1d6759f648cf8d5294a40
oliegjio <oliegjio@gmail.com>
2018-10-24 10:32:25 +0000

    Finished Mandelbrot set

--- main.cpp
+++ main.cpp
@@ -1,18 +1,46 @@
 #include <GL/glut.h>
+#include <complex>
+#include <iostream>
 
 #define WIN_WIDTH 800
-#define WIN_HEIGHT 600
-
-void setup()
-{
-
-}
+#define WIN_HEIGHT 800
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    std::complex<float> z;
+    std::complex<float> c;
 
+    const float step = 0.005;
+    const float jumps = 20.0f;
+
+    glBegin(GL_POINTS);
+    for (float i = -2.0f; i <= 2.0f; i += step)
+    {
+        for (float j = -2.0f; j <= 2.0f; j += step)
+        {
+            z = {0, 0};
+            c = {i, j};
+            for (float k = 1; k <= jumps; k++)
+            {
+                z = (z * z) + c;
+                if (std::abs(z) > 8.0f)
+                {
+                    glColor3f(k / jumps, 0.0f, 0.0f);
+                    glVertex2f(c.real(), c.imag());
+                    break;
+                }
+                if (k == jumps)
+                {
+                    glColor3f(0.0f, 0.0f, 0.0f);
+                    glVertex2f(c.real(), c.imag());
+                    break;
+                }
+            }
+        }
+    }
+    glEnd();
 
     glutSwapBuffers();
 }
@@ -31,15 +59,13 @@ void reshape(int width, int height)
 
 int main(int argc, char **argv)
 {
-    setup();
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
     glutCreateWindow("Mandelbrot Set");
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
